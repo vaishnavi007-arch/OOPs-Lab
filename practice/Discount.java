379b872554e261d1fab1b4956331e89e917da562
vaishnavi007-arch <vaishnavivenkit@gmail.com>
2026-01-29 14:19:52 +0530

    Add files via upload

---
+++ practice/Discount.java
@@ -0,0 +1,10 @@
+class Discount{
+public static void main(String args[]){
+String item = "KitKat chocolate";
+double amount = 20.0;
+int dp = 5;
+boolean dis = true;
+System.out.println("ITEM: "+item);
+System.out.println("AMOUNT: "+amount);
+System.out.println("DISCOUNT%: "+dp);
+System.out.println("DISCOUNT AVAILED: "+dis);}}
