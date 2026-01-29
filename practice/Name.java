ec3587f776a82661219c74091d9216aadc10448d
vaishnavi007-arch <vaishnavivenkit@gmail.com>
2026-01-29 14:16:01 +0530

    Add files via upload

---
+++ practice/Name.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Name{
+	public static void main(String[] args){
+	Scanner myObj= new Scanner(System.in);
+	String name= myObj.nextLine();
+	System.out.println(name);
+	}
+}
